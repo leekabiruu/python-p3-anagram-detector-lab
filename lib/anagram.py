d22a70db2698dc2a6abf1eb59a217cffbb7b83ab
Lee <leekinyuakabiru@gmail.com>
2025-08-10 20:44:33 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.word_lower = word.lower()
+        self.sorted_word = sorted(self.word_lower)
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            lower_word = word.lower()
+            if lower_word != self.word_lower and sorted(lower_word) == self.sorted_word:
+                matches.append(word)
+        return matches
+
+print(Anagram("enlist").match(["listen", "silent", "hippopotamus"])) 
